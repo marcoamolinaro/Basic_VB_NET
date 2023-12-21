94984c9e29caaf72c7bc0b5286ed260339565280
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-20 23:34:20 -0300

    My first program

---
+++ MyFirstProject/MyFirstProject/Module1.vb
@@ -0,0 +1,11 @@
+﻿Imports System
+
+Module Module1
+
+    Sub Main(args As String())
+
+        Console.WriteLine("Hello World!")
+
+    End Sub
+
+End Module
---
+++ MyFirstProject/MyFirstProject/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' As informações gerais sobre um assembly são controladas por
+' conjunto de atributos. Altere estes valores de atributo para modificar as informações
+' associadas a um assembly.
+
+' Revise os valores dos atributos do assembly
+
+<Assembly: AssemblyTitle("MyFirstProject")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("MyFirstProject")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+<Assembly: Guid("ff7e8df6-634b-4b11-9f03-d4ec8d5febe3")>
+
+' As informações da versão de um assembly consistem nos quatro valores a seguir:
+'
+'      Versão Principal
+'      Versão Secundária 
+'      Número da Versão
+'      Revisão
+'
+' É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+' usando o "*" como mostrado abaixo:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
