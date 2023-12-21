11c03e7a74b9955d694285c9f9986747eeb02495
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-21 15:15:27 -0300

    Getting user input thru console

--- MyFirstProject1/MyFirstProject1/Program.vb
+++ MyFirstProject1/MyFirstProject1/Program.vb
@@ -4,30 +4,12 @@ Module Program
     Sub Main(args As String())
         Dim val1 As Integer = 30
         Dim val2 As Double = 4
-
-        'While val1 <= 20
-        '    If val1 = 10 Then
-        '        Exit While
-        '    End If
-        '    Console.WriteLine(val1 & " modulus " & val2 & " is " & (val1 Mod val2) & ".")
-        '    val1 += 1
-        'End While
-
-        'Do
-        '    If val1 = 10 Then
-        '        Exit Do
-        '    End If
-        '    Console.WriteLine(val1 & " modulus " & val2 & " is " & (val1 Mod val2) & ".")
-        '    val1 += 1
-        'Loop While val1 <= 20
-
-        Do
-            If val1 = 100 Then
-                Exit Do
-            End If
-            Console.WriteLine(val1 & " modulus " & val2 & " is " & (val1 Mod val2) & ".")
-            val1 += 1
-        Loop Until val1 <= 20
+        Dim userInput As String
+        Console.Write("Enter a number ")
+        userInput = Console.ReadLine()
+        Console.WriteLine(userInput)
+        val2 = CDbl(userInput)
+        Console.WriteLine("Soma " & (val1 + val2))
     End Sub
 End Module
 
