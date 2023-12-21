014863369f7a46aa8f36334725b6871500cc470a
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-21 15:06:14 -0300

    while loops and do while

--- MyFirstProject1/MyFirstProject1/Program.vb
+++ MyFirstProject1/MyFirstProject1/Program.vb
@@ -2,46 +2,32 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim val1 As Integer
+        Dim val1 As Integer = 30
         Dim val2 As Double = 4
 
-        'For val1 = 1 To 20
+        'While val1 <= 20
+        '    If val1 = 10 Then
+        '        Exit While
+        '    End If
         '    Console.WriteLine(val1 & " modulus " & val2 & " is " & (val1 Mod val2) & ".")
-        'Next
+        '    val1 += 1
+        'End While
 
-        'Console.WriteLine("The value for val1 is " & val1)
-
-        'For val1 = 30 To 20 Step -1
+        'Do
+        '    If val1 = 10 Then
+        '        Exit Do
+        '    End If
         '    Console.WriteLine(val1 & " modulus " & val2 & " is " & (val1 Mod val2) & ".")
-        'Next
+        '    val1 += 1
+        'Loop While val1 <= 20
 
-        'For val3 As Double = 1 To 20 Step 0.1
-        '    Console.WriteLine(val3 & " modulus " & val2 & " is " & (val3 Mod val2) & ".")
-        'Next
-
-        'For val3 As Decimal = 1 To 20 Step 1
-        '    Select Case val3
-        '        Case 10
-        '        Case Else
-        '            Console.WriteLine(val3 & " The modulus " & val2 & " is " & (val3 Mod val2) & ".")
-        '    End Select
-        'Next
-
-        For val3 As Decimal = 1 To 20 Step 1
-            If val3 = 10 Then
-                Continue For
+        Do
+            If val1 = 100 Then
+                Exit Do
             End If
-
-            Console.WriteLine(val3 & " The modulus " & val2 & " is " & (val3 Mod val2) & ".")
-        Next
-
-        For val3 As Decimal = 1 To 20 Step 1
-            If val3 = 10 Then
-                Exit For
-            End If
-
-            Console.WriteLine(val3 & " The modulus " & val2 & " is " & (val3 Mod val2) & ".")
-        Next
+            Console.WriteLine(val1 & " modulus " & val2 & " is " & (val1 Mod val2) & ".")
+            val1 += 1
+        Loop Until val1 <= 20
     End Sub
 End Module
 
