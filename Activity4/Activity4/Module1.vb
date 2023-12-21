aba34a07041f433d40d83d1298cf04829a332ec3
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-21 15:42:56 -0300

    activity 5

---
+++ Activity4/Activity4/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim UserInput As String
+        Dim RunningTotal As Double = 1
+        Do
+            Console.Write("Please enter your number ")
+            UserInput = Console.ReadLine()
+            If UserInput = "0 " Then
+                Exit Do
+            End If
+            RunningTotal *= UserInput
+        Loop Until UserInput <> "0"
+        Console.Write("Your answer is ")
+        Console.WriteLine(RunningTotal)
+    End Sub
+
+End Module
