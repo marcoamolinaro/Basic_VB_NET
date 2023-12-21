5520720f784768618809e62e65e710c8a26c22af
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-20 23:52:15 -0300

    My First Activity

---
+++ MyFirstActivity/MyFirstActivity/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        'This is my first Practice Activity.
+        Console.WriteLine("Hello World!")
+        Console.WriteLine("This is my first program")
+
+    End Sub
+
+End Module
