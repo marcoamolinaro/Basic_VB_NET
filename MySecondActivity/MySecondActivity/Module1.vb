bec0533c37327d60b10dfe80a7ee800d050ded17
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-21 09:13:50 -0300

    My second activity

---
+++ MySecondActivity/MySecondActivity/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim var1 As String = "John"
+        Dim var2 As String = "Smith"
+        Dim var3 As Short = 1
+
+        Console.WriteLine("Hello " & var1 & " " & var2 & vbCrLf & "This is my Program Number " & var3)
+
+        var1 = "Mary"
+        var2 = "Jones"
+        var3 = 2
+
+        Console.WriteLine("Hello " & var1 & " " & var2 & vbCrLf & "This is my Program Number " & var3)
+
+    End Sub
+
+End Module
