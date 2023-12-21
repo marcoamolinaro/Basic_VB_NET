4967df9792d4226967319b51665f4d0c313bf399
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-21 09:06:10 -0300

    Other types of variable types

--- MyFirstProject1/MyFirstProject1/Program.vb
+++ MyFirstProject1/MyFirstProject1/Program.vb
@@ -2,21 +2,29 @@ Imports System
 
 Module Program
     Sub Main(args As String())
-        Dim val1 As Single = 2.5
-        Dim val2 As Single = 3.5
-        Console.WriteLine("My numerbers are " & val1 & " and " & val2 & "!" &
-                          vbCrLf & "The tatal is " & val1 + val2)
 
-        Dim val3 As Decimal = 123456789123456789.1234D
-        Dim val4 As Decimal = 0
-        Console.WriteLine("My numerbers are " & val3 & " and " & val4 & "!" &
-                          vbCrLf & "The tatal is " & val3 + val4)
+        Dim val5 As Boolean = True
+        Console.WriteLine("The value is " & val5)
+        Dim val6 As Boolean
+        Console.WriteLine("The value is " & val6)
+
+        Dim val7 As String = "This is my String"
+        Console.WriteLine("The value is " & val7)
+
+        val7 &= " And this is appended String"
+        Console.WriteLine("The value is " & val7)
+
+        Dim val8 As Char = "A"c
+        Console.WriteLine("The value is char " & val8)
+
+        Dim val9 As Date = #2023/11/22#
+        Console.WriteLine("The value is a Date " & val9)
+
+        val9 = #2023/11/22 17:15:00#
+        Console.WriteLine("The value is a Date " & val9)
+
+
+
     End Sub
 End Module
 
-' Byte  0 to 255                            (3 digits)      - 1 byte
-' Short -32,768 to +32,768                  (5 digits)      - 2 bytes
-' UShort 0 to +65,535
-' Integer -2 billion to +2 billion          (10 digits)     - 4 bytes
-' Long  -9 quintillion to +9 quintillion    (19 digits)     - 8 bytes
-' BaSIL LISB
