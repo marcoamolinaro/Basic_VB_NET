4fa3e57b31ee3842ed7be5252ad4e154b07f8b7b
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-21 14:55:31 -0300

    For Next loops

--- MyFirstProject1/MyFirstProject1/Program.vb
+++ MyFirstProject1/MyFirstProject1/Program.vb
@@ -2,20 +2,46 @@ Imports System
 
 Module Program
     Sub Main(args As String())
+        Dim val1 As Integer
+        Dim val2 As Double = 4
 
-        Dim val1 As Integer = 2
+        'For val1 = 1 To 20
+        '    Console.WriteLine(val1 & " modulus " & val2 & " is " & (val1 Mod val2) & ".")
+        'Next
 
-        If val1 = 2 Then
-            Console.WriteLine("This condition has been met.")
-        End If
+        'Console.WriteLine("The value for val1 is " & val1)
 
-        If Not 3 = 2 Then
-            Console.WriteLine("This condition has been met.")
-        End If
+        'For val1 = 30 To 20 Step -1
+        '    Console.WriteLine(val1 & " modulus " & val2 & " is " & (val1 Mod val2) & ".")
+        'Next
 
+        'For val3 As Double = 1 To 20 Step 0.1
+        '    Console.WriteLine(val3 & " modulus " & val2 & " is " & (val3 Mod val2) & ".")
+        'Next
 
-        Console.WriteLine("This the end of the program.")
+        'For val3 As Decimal = 1 To 20 Step 1
+        '    Select Case val3
+        '        Case 10
+        '        Case Else
+        '            Console.WriteLine(val3 & " The modulus " & val2 & " is " & (val3 Mod val2) & ".")
+        '    End Select
+        'Next
 
+        For val3 As Decimal = 1 To 20 Step 1
+            If val3 = 10 Then
+                Continue For
+            End If
+
+            Console.WriteLine(val3 & " The modulus " & val2 & " is " & (val3 Mod val2) & ".")
+        Next
+
+        For val3 As Decimal = 1 To 20 Step 1
+            If val3 = 10 Then
+                Exit For
+            End If
+
+            Console.WriteLine(val3 & " The modulus " & val2 & " is " & (val3 Mod val2) & ".")
+        Next
     End Sub
 End Module
 
