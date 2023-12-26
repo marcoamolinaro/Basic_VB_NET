726ca663c99dc2a49eb2649dee1a25e31ec4fcdb
Marco A Molinaro <molinaromarcoaurelio@gmail.com>
2023-12-26 10:29:35 -0300

    Events

--- MyFirstWindowsFormApp/MyFirstWindowsFormApp/Form1.vb
+++ MyFirstWindowsFormApp/MyFirstWindowsFormApp/Form1.vb
@@ -7,4 +7,15 @@
             MessageBox.Show("I cannot add these numbers together!")
         End Try
     End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        MsgBox("Load")
+    End Sub
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        MsgBox("Shown")
+    End Sub
+
+    Private Sub Form1_Activated(sender As Object, e As EventArgs) Handles MyBase.Activated
+    End Sub
 End Class
